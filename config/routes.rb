52547d3b6234a46a59be383ee16a4945feada8a1
blaineboudreau <blaine.boudreau@gmail.com>
2017-02-16 04:19:37 -0600

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  root 'welcome#index'
   # resources :games
   resources :users, only: [:index, :show, :update, :create, :destroy, :search] do
     collection do
